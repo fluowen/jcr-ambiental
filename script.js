67066abadd5528bbdd44fe51c62977fd00e685dc
Fluowen <contact@fluowen.com>
2024-09-24 17:54:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Carrossel Automático
+let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+}
+
+// Alterar slides a cada 2 segundos
+setInterval(nextSlide, 2000);

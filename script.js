705a6aec20f1df13c17b32eb911a29f7b04a09f9
Fluowen <contact@fluowen.com>
2024-10-05 11:58:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-// JavaScript para controlar o carrossel entre Missão e Visão
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-item');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.display = i === index ? 'block' : 'none';
-    });
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
-}
-
-// Mudar de slide a cada 5 segundos
-setInterval(nextSlide, 5000);
+// Carrossel irá mudar os slides automaticamente a cada 5 segundos
+var myCarousel = document.querySelector('#carouselExampleIndicators')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 5000,
+  wrap: true
+})

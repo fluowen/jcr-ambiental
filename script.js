5e2f67a09ca59d5b0d3fe2fbb21f6de5e8a2a05f
Fluowen <contact@fluowen.com>
2024-10-05 12:00:55 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Carrossel irá mudar os slides automaticamente a cada 5 segundos
-var myCarousel = document.querySelector('#carouselExampleIndicators')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 5000,
-  wrap: true
-})

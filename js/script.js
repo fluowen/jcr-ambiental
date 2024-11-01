4ee7027464a47bee76ace21a99e6e0e58489acfa
Fluowen <contact@fluowen.com>
2024-11-01 15:10:36 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 // script.js
 
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
 
-// Close overlay when any link is clicked
-const overlayLinks = document.querySelectorAll('.overlay-content a');
-overlayLinks.forEach(link => {
-  link.addEventListener('click', function() {
-      closeNav();
-  });
+    // Close overlay when any link is clicked
+    const overlayLinks = document.querySelectorAll('.overlay-content a');
+    overlayLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            closeNav();
+        });
+    });
+
+    // Attach openNav to the button
+    document.querySelector('.overlay-menu-toggler').addEventListener('click', openNav);
+    // Attach closeNav to the close button
+    document.querySelector('.closebtn').addEventListener('click', closeNav);
 });
